6c733c368cb7255794bbecc8a7ee69813663b8dd
Justin Abioye <Justinabioye@gmail.com>
2023-04-22 12:00:49 +0530

    changes done

--- tel.py
+++ tel.py
@@ -1,7 +1,7 @@
 import telegram.ext 
 
 def start(update, context):
-    update.message.reply_text("Hello! Welcome to Simplilearn")
+    update.message.reply_text("Hello! Welcome to RoomsForLiving Type help")
     
 def help(update,context):
     update.message.reply_text("""
@@ -9,48 +9,47 @@ def help(update,context):
     
     /start -> Welcome to the channel
     /help -> This message
-    /content -> Simplilearn offers various courses free of course through Skillup by Simplilearn
-    /Python  -> The first video from Python Playlist
-    /SQL -> The first video from SQL Playlist
+    /Room -> Check Room Availability
+    /Price  -> Room Price
+    /Location -> Google Map Location
     /Java -> The first video from Java Playlist
     /Skillup -> Free platform for certification by Simplilearn
     /contact -> contact information 
      """)
     
-def content(update, context):
-    update.message.reply_text("We have various playlists and articles available!")
+def Room(update, context):
+    update.message.reply_text("We have Rooms")
 
-def Python(update, context):
-    update.message.reply_text("tutorial link : https://youtu.be/Tm5u97I7OrM")
+def Price(update, context):
+    update.message.reply_text("Room is 4,500 ")
 
-def SQL(update, context):
-    update.message.reply_text("tutorial link : https://youtu.be/pFq1pgli0OQ")
+def Location(update, context):
+    update.message.reply_text("Click for Location : https://maps.app.goo.gl/BErMXJ4X43VK1EAi8 ")
     
 def Java(update, context):
     update.message.reply_text("tutorial link : https://youtu.be/i6AZdFxTK9I")
     
-def Skillup(update, context):
-    update.message.reply_text("tutorial link : https://www.simplilearn.com/?&utm_source=google&utm_medium=cpc&utm_term=%2Bwww%20%2Bsimplilearn%20%2Bcom&utm_content=803350713-40537012023-467574577661&utm_device=c&utm_campaign=Search-Brand-Broad-IN-AllDevice-adgroup-brand-navigation&gclid=Cj0KCQjw0oyYBhDGARIsAMZEuMv5mA9EysZZ5NfhK65Cb5OU0Q0TVC4con6DQzHF502-dfgA7QQFCgQaAu5sEALw_wcB")
+def Images(update, context):
+    update.message.reply_text("tutorial link :")
     
 def contact(update, context):
-    update.message.reply_text("You can contact on the official mail id")
+    update.message.reply_text("You can contact on the official mailgitgiid")
 
 def handle_message(update, context):
     update.message.reply_text(f"You said {update.message.text}, use the commands using /")
 
-
-Token = ("Paste your Telegram bot token here")
+Token = ("6217926804:AAEdahmsm9_gCE6p7dtbmEeXZRzBwMjOYaU")
 #print(bot.get_me())
-updater = telegram.ext.Updater("Paste your Telegram bot token here", use_context=True)
+updater = telegram.ext.Updater("6217926804:AAEdahmsm9_gCE6p7dtbmEeXZRzBwMjOYaU", use_context=True)
 disp = updater.dispatcher
 
 disp.add_handler(telegram.ext.CommandHandler('start',start))
 disp.add_handler(telegram.ext.CommandHandler('help',help))
-disp.add_handler(telegram.ext.CommandHandler('content',content))
-disp.add_handler(telegram.ext.CommandHandler('Python',Python))
-disp.add_handler(telegram.ext.CommandHandler('SQL',SQL))
+disp.add_handler(telegram.ext.CommandHandler('Room',Room))
+disp.add_handler(telegram.ext.CommandHandler('Price',Price))
+disp.add_handler(telegram.ext.CommandHandler('Location',Location))
 disp.add_handler(telegram.ext.CommandHandler('Java',Java))
-disp.add_handler(telegram.ext.CommandHandler('Skillup',Skillup))
+disp.add_handler(telegram.ext.CommandHandler('Images',Images))
 disp.add_handler(telegram.ext.CommandHandler('contact',contact))
 disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
 updater.start_polling()
